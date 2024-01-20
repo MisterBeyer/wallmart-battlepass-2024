9995b26ff5b19ea30568732832cd498d314765fd
B0gos <admin@perhapsbot.dev>
2024-01-20 01:52:39 +0000

    Intake WIP

---
+++ src/main/java/frc/robot/commands/intake/IntakeSpeed.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.intake;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.intake;
+
+//dont unplug the ethernet
+public class IntakeSpeed extends Command {
+    private intake intake;
+    private double speed;
+
+    public IntakeSpeed(intake module, double speed) {
+        this.speed = speed; 
+        this.intake = module;
+        addRequirements(this.intake);
+
+        SmartDashboard.putNumber("Intake Speed", speed);
+    }
+
+      // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        intake.setspeed(speed);
+    }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        intake.stop();
+        }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+        // Don't know any end conditions to add here, hoping RobotContainer just interupts it
+    }
+}
