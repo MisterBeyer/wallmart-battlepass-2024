eaa057e49a1f1d2a8931d7f5b567efb6916d0c7c
Not-The-Skinwalker-Oliver <admin@perhapsbot.dev>
2024-02-17 15:44:00 -0800

    started custom rgb animation

--- src/main/java/frc/robot/subsystems/Bluetooth.java
+++ src/main/java/frc/robot/subsystems/Bluetooth.java
@@ -11,6 +11,9 @@ public class Bluetooth extends SubsystemBase{
     private static boolean toggle = false;
     // Bofa these bluetooth
 
+    private static boolean th5 = false;
+    //town hall 5 moment
+
     public Bluetooth(){
         }
         static CANdle candle = new CANdle(40);
@@ -30,7 +33,23 @@ public class Bluetooth extends SubsystemBase{
         ;return toggle;
     }
 
+public boolean th5() {
+  th5 = !th5
+  ;if(th5){ setColor(255, 50, 2)
+  ;try {
+    wait(2000)
+    ;
+  } catch (InterruptedException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }candle.clearAnimation(0)
+  ;candle.setLEDs(0, 0, 0)
+  ;candle.setLEDs(255, 0, 0)
+;}
+  else bluetoothOFF()
 
+  ;return th5;
+}
     /**
      * Sets color of the whole rgb strip
      * @param r Red brightness as int (0-255)
