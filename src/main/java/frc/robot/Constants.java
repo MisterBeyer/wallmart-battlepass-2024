93064b6c9c388a17595e48e144fb426f8982b64a
Not-The-Skinwalker-Oliver <admin@perhapsbot.dev>
2024-02-21 20:02:11 -0800

    Small changes, rpm added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,7 @@ public final class Constants
     public static double IntakeSpeed = 0.1; 
     public static double BackOut = 0.8;
     public static double FrontOut = 0.8;
-    public static double FrontRPM = 762.7;
+    public static double FrontRPM = 2500;
     public static int BackRPM = 0;
 
     // Arm/Wrist Motor Speed and amp limits
