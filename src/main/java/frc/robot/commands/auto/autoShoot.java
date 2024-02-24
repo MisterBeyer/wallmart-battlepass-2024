44f8a1937a9d43a90ead6a9033335c1d00798198
B0gos <admin@perhapsbot.dev>
2024-02-23 16:36:37 -0800

    Moved IntakeShoot into Helper Commands

--- src/main/java/frc/robot/commands/auto/autoShoot.java
+++ src/main/java/frc/robot/commands/auto/autoShoot.java
@@ -2,7 +2,7 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj2.command.Command;
 //import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.Drivebase;
-import frc.robot.commands.intake.IntakeShoot;
+import frc.robot.commands.Helpers.IntakeShoot;
 import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj.Timer;
 
