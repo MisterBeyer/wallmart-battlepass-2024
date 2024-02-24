8332427b791ee46078d33b1cbffeec0a6a4da0cb
B0gos <CTBT>
2024-02-24 14:11:08 -0800

    Fix wrist direct commands, added incomplete rumble

--- src/main/java/frc/robot/subsystems/Rumble.java
+++ src/main/java/frc/robot/subsystems/Rumble.java
@@ -3,10 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OperatorConstants;
 
-public class Rumble extends SubsystemBase {
+public class Rumble extends SubsystemBase{
     private XboxController driver;
     private XboxController operator;
 
@@ -21,12 +22,25 @@ public class Rumble extends SubsystemBase {
     }
 
     /* Shakes Driver Controller */
-   // public Command driver() {
-     //   return driver.setRumble(GenericHID.RumbleType.kBothRumble, OperatorConstants.strength);
+    public Command driver() {
+        return Commands.runOnce(() -> v_ShortDriver(), this);
     }
 
     /* Shakes Operator Controller */
-   // public Command operator() {
-     //   return operator.setRumble(GenericHID.RumbleType.kBothRumble, OperatorConstants.strength);
-    //}
-//}
+    public Command operator() {
+       return Commands.runOnce(() -> v_ShortOperator(), this);
+    }
+
+
+    private void v_ShortDriver() {
+        driver.setRumble(GenericHID.RumbleType.kBothRumble, OperatorConstants.RumbleStrength);
+
+        driver.setRumble(GenericHID.RumbleType.kBothRumble, 0.0);
+    }
+
+    private void v_ShortOperator() {
+        driver.setRumble(GenericHID.RumbleType.kBothRumble, OperatorConstants.RumbleStrength);
+
+        driver.setRumble(GenericHID.RumbleType.kBothRumble, 0.0);
+    }
+}
