9b8fb251f28b97691bcab2b9f0d1f9bfb9a8e12c
B0gos <CTBT>
2024-02-29 18:15:46 +0000

    Added the rest of Auto Operator Commands

--- src/main/java/frc/robot/commands/Helpers/Intake/ShootRampUp.java
+++ src/main/java/frc/robot/commands/Helpers/Intake/ShootRampUp.java
@@ -1,7 +1,7 @@
 package frc.robot.commands.Helpers.Intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.Intake;
 
