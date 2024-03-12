4d525ec4ab2a69b6ca16c058c312005c6511e39c
B0gos <admin@perhapsbot.dev>
2024-03-12 16:06:51 -0700

    Flipped Camra on Shuffleboard

--- src/main/java/frc/robot/commands/Helpers/Intake/ShootRampUp.java
+++ src/main/java/frc/robot/commands/Helpers/Intake/ShootRampUp.java
@@ -75,12 +75,8 @@ public class ShootRampUp extends Command {
                 intake.setSpeed(-OperatorConstants.FrontOut, OperatorConstants.BackOut);
             }
             else state = 5;
-        } 
-<<<<<<< HEAD
-        System.out.println(state + " " + intake.getFrontCurrent());
-=======
+        }
         System.out.println("state: "+state+" - " + intake.getFrontCurrent());
->>>>>>> d827ba58a3360da4b9dc197b2bcc97e4c6ba0c70
     }
 
 
