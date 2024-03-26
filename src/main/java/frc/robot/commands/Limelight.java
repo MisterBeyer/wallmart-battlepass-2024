35c3cb3d954269770ddcb57b8b7c1ca50873c49f
Not-The-Skinwalker-Oliver <CTBT>
2024-03-26 13:12:45 -0700

    added todo to limelight.java

--- src/main/java/frc/robot/commands/Limelight.java
+++ src/main/java/frc/robot/commands/Limelight.java
@@ -3,3 +3,5 @@ package frc.robot.commands;
 public class Limelight {
     
 }
+
+// TODO: to start lets get the limelight to see (ex: ID 1) and do a simple task (ex: raise arm to speaker or spin 180)
