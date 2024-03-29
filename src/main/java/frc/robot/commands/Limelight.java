3fb73031d48aa9af3c178ff08c50a20db87b98fa
Elizabeth <lopez.elizabeth.alica@gmail.com>
2024-03-29 15:57:35 -0700

    limelight

--- src/main/java/frc/robot/commands/Limelight.java
+++ src/main/java/frc/robot/commands/Limelight.java
@@ -2,18 +2,45 @@ package frc.robot.commands;
 
 import frc.robot.LimelightHelpers;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class Limelight {
+public class Limelight extends Command {
+    private double tx;
+    private double ty;
+    private double area;
 
-    //read values periodically
-    double x = LimelightHelpers.getTX("");
-    double y = LimelightHelpers.getTY("");
-    double area = LimelightHelpers.getTX("");
-    
-    //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
+
+    public Limelight() {
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("[LimeLight] Bofa Mode Loaded");
+    }
+
+    @Override
+    public void execute() {
+        //read values periodically
+        tx = LimelightHelpers.getTX("");
+        ty = LimelightHelpers.getTY("");
+        area = LimelightHelpers.getTA("");
+        
+        //post to smart dashboard periodically
+        SmartDashboard.putNumber("Limelight/TX", tx);
+        SmartDashboard.putNumber("Limelight/TY", ty);
+        SmartDashboard.putNumber("Limelight/Area", area);
+    }
+
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override 
+    public boolean isFinished() {
+        return false;
+    }
 }
+
 
 // TODO: to start lets get the limelight to see (ex: ID 1) and do a simple task (ex: raise arm to speaker or spin 180)
