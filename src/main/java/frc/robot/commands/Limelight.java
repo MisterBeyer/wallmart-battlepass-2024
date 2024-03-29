b94bacd7437f7a500ab2f5b5e2d5d4cb871e9d9e
B0gos <admin@perhapsbot.dev>
2024-03-29 13:53:18 -0700

    Redcutions to Limelight

--- src/main/java/frc/robot/commands/Limelight.java
+++ src/main/java/frc/robot/commands/Limelight.java
@@ -1,30 +1,19 @@
 package frc.robot.commands;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableValue;
 import frc.robot.LimelightHelpers;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
-    NetworkTable Table = NetworkTable.getTable("Limelight");
-    
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    
+
     //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
+    double x = LimelightHelpers.getTX("");
+    double y = LimelightHelpers.getTY("");
+    double area = LimelightHelpers.getTX("");
     
     //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX". x);
-    SmartDashboard.putNumber("LimelightY". y);
-    SmartDashboard.putNumber("LimelightArea". area);
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
 }
 
 // TODO: to start lets get the limelight to see (ex: ID 1) and do a simple task (ex: raise arm to speaker or spin 180)
