1a4c8bd219981a19cd1818974467e2c5f15ae9e3
Not-The-Skinwalker-Oliver <admin@perhapsbot.dev>
2024-04-02 09:12:06 -0700

    added Tv

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LimeLight;
 
 /* // TODO: Make Sure Robot didn't explod
 import edu.wpi.first.wpilibj.Filesystem;
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot
 
   private static Robot   instance;
   private        Command m_autonomousCommand;
+  private final LimeLight Limelight = new LimeLight();
 
   private RobotContainer m_robotContainer;
 
