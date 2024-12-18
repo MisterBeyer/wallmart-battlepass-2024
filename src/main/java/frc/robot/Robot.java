08ad5d48c14fdcd703052988f7def707ee5de76c
elle <lopez.elizabeth.alica@gmail.com>
2024-12-17 16:25:02 -0800

    slight changes for advantagekit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,12 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+
 package frc.robot;
+
+import org.littletonrobotics.junction.LoggedRobot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 
 
 /**
@@ -16,8 +21,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project, you must also update the build.gradle file in the project.
  */
 
-public class Robot extends TimedRobot
+public class Robot extends LoggedRobot
 {
+  
 
   private static Robot   instance;
   private        Command m_autonomousCommand;
